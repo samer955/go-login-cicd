87679b28ae3dc3134efe21d142ea038643cae943
samer955 <45147306+samer955@users.noreply.github.com>
2023-02-07 16:49:23 +0100

    Update update_app_version.sh

--- update_app_version.sh
+++ update_app_version.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Set the desired version number
+#Goal of this script is to update after every push the image:tag of the kubernetes Manifest in order to trigger argoCD and update the running app
+on kubernetes (Continuous Delivery).
+
+# Env variables passed from the CI Pipeline (Github Workflow).
 APP_VERSION=$1
 GITHUB_TOKEN=$2
 GITHUB_ACTOR=$3
 
 
-
 # Clone the repository that contains the manifest
+echo "Cloning the repository containing the manifest"
 git clone https://github.com/samer955/argocd-config-login.git repo
 cd repo
 
 # Replace the old version with the new version in the manifest
+echo "Updating the Manifest to version $APP_VERSION"
 sed -i "s/image: ghcr.io\/samer955\/go-login-cicd:.*/image: ghcr.io\/samer955\/go-login-cicd:$APP_VERSION/" dev/deployment.yaml
 
 # Set the remote URL to use the GITHUB_TOKEN secret
@@ -27,7 +31,10 @@ git commit -m "Update version in manifest to $APP_VERSION"
 
 # Push changes to the repository
 git push origin main
+echo "Pushed changes to the repo"
 
 # Clean up
 cd ..
 rm -rf repo
+
+echo "Done"
