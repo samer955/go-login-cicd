25e74b4ccf9b53c7b0ab745d2d85fe90463f7d6e
samer955 <45147306+samer955@users.noreply.github.com>
2023-02-07 15:40:24 +0100

    Update update_app_version.sh

--- update_app_version.sh
+++ update_app_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the desired version number
-app_version=$1
+APP_VERSION=$1
 GITHUB_TOKEN=$2
 
 
@@ -11,7 +11,7 @@ git clone https://github.com/samer955/argocd-config-login.git repo
 cd repo
 
 # Replace the old version with the new version in the manifest
-sed -i "s/image: ghcr.io\/samer955\/go-login-cicd:.*/image: ghcr.io\/samer955\/go-login-cicd:$app_version/" dev/deployment.yaml
+sed -i "s/image: ghcr.io\/samer955\/go-login-cicd:.*/image: ghcr.io\/samer955\/go-login-cicd:$APP_VERSION/" dev/deployment.yaml
 
 # Set the remote URL to use the GITHUB_TOKEN secret
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/samer955/argocd-config-login.git
@@ -22,7 +22,7 @@ git config user.name "samer955"
 
 # Add changes and commit
 git add dev/deployment.yaml
-git commit -m "Update version in manifest to $app_version"
+git commit -m "Update version in manifest to $APP_VERSION"
 
 # Push changes to the repository
 git push origin main
