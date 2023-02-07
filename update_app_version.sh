ff5902d6eb7247f70ec55652e1a5ccebd2296062
samer955 <45147306+samer955@users.noreply.github.com>
2023-02-07 15:33:55 +0100

    Update update_app_version.sh

--- update_app_version.sh
+++ update_app_version.sh
@@ -14,7 +14,7 @@ cd repo
 sed -i "s/image: ghcr.io\/samer955\/go-login-cicd:.*/image: ghcr.io\/samer955\/go-login-cicd:$app_version/" dev/deployment.yaml
 
 # Set the remote URL to use the GITHUB_TOKEN secret
-git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/github.com/samer955/argocd-config-login.git
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/samer955/argocd-config-login.git
 
 # Commit and push the changes to the repository
 git config user.email "samer.osman95@hotmail.com"
